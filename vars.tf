2ca739d5da084f356d517b469f599c1e917e6ea6
Paulo Neves <pneves@saqja.com.br>
2023-05-24 23:42:41 -0300

    dlq 2

--- vars.tf
+++ vars.tf
@@ -5,36 +5,8 @@ variable "topic_and_queue_subscription_name" {
   }))
   default = [
     {
-      sns_topic_name           = "transaction-full"
-      queue_subscripted_name   = "withdraw-new-transaction"
-    },
-    {
-      sns_topic_name           = "withdraw-split-status"
-      queue_subscripted_name   = "withdraw-split-status"
-    },
-    {
-      sns_topic_name           = "transactions-bankly-updated"
-      queue_subscripted_name   = "transactions-bankly-updated"
-    },
-    {
-      sns_topic_name           = "withdraw-payment-status"
-      queue_subscripted_name   = "withdraw-payment-status"
-    },
-    {
-      sns_topic_name           = "pix-created"
-      queue_subscripted_name   = "pix-created"
-    },
-    {
-      sns_topic_name           = "qrcode-bankly-created"
-      queue_subscripted_name   = "qrcode-bankly-created"
-    },
-    {
-      sns_topic_name           = "qrcode-bankly-updated"
-      queue_subscripted_name   = "qrcode-bankly-updated"
-    },
-    {
-      sns_topic_name           = "update-stone-transaction-topic"
-      queue_subscripted_name   = "update-stone-transaction-queue"
+      sns_topic_name           = "sns-1"
+      queue_subscripted_name   = "sqs-queue2"
     }
   ]
 }
